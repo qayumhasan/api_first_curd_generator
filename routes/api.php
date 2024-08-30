8a59f0a5fa90955c23ebb86f4b6969011b9f1959
qayumhasan <dev.qayumhasan@gmail.com>
2024-08-30 23:57:33 +0600

    chor:update the composer file

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('newtests', App\Http\Controllers\Api\NewTestController::class);
-Route::apiResource('newtestcruds', App\Http\Controllers\Api\NewTestCrudController::class);
-Route::apiResource('hellocurds', App\Http\Controllers\Api\HelloCurdController::class);
